0bde6000817e470a54bbd10a7d4350c8131b7548
lyhuynhduybao <lyhuynhduybao@gmail.com>
2019-05-08 17:26:57 +0700

    Build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,16 @@ export class AppComponent {
         thumbnail: "assets/1.jpg"
       },
       {
-        image: "assets/1.jpg",
-        thumbnail: "assets/1.jpg"
+        image: "assets/2.jpg",
+        thumbnail: "assets/2.jpg"
+      },
+      {
+        image: "assets/3.jpg",
+        thumbnail: "assets/3.jpg"
+      },
+      {
+        image: "assets/4.jpg",
+        thumbnail: "assets/4.jpg"
       },
     ];
 
